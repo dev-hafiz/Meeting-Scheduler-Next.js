b0cd9588941e36d7aaa468540b53b284592a04df
dev-hafiz <johnstarhafiz@gmail.com>
2024-08-18 20:04:29 +0600

    Create Header succussfully with mobile responsive

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,10 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
 
 const Header = () => {
   return (
     <div>
-      <div>
+      <div className="flex items-center justify-between p-5 shadow-sm">
         <Image
           src="/logo.svg"
           width={100}
@@ -12,6 +13,24 @@ const Header = () => {
           alt="logo"
           className="w-[150px] md:w-[200px]"
         />
+        <ul className="hidden md:flex gap-14 font-medium text-lg">
+          <li className="hover:text-primary transition-all duration-300 cursor-pointer">
+            Product
+          </li>
+          <li className="hover:text-primary transition-all duration-300 cursor-pointer">
+            Pricing
+          </li>
+          <li className="hover:text-primary transition-all duration-300 cursor-pointer">
+            Contuct us
+          </li>
+          <li className="hover:text-primary transition-all duration-300 cursor-pointer">
+            About us
+          </li>
+        </ul>
+        <div className="flex gap-5">
+          <Button variant="ghost">Login</Button>
+          <Button>Get Started</Button>
+        </div>
       </div>
     </div>
   );
