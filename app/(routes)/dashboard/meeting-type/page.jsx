df2c956924d45c4073041275a81e34931da76f1e
dev-hafiz <johnstarhafiz@gmail.com>
2024-08-23 20:01:32 +0600

    In Meeting type page, I load all created event from firebase database and console it successfully and then I will show on UI

--- app/(routes)/dashboard/meeting-type/page.jsx
+++ app/(routes)/dashboard/meeting-type/page.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+"use client";
+import { Input } from "@/components/ui/input";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import { app } from "@/config/FirebaseConfig";
+import React, { useEffect } from "react";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 const MeetingType = () => {
+  const { user } = useKindeBrowserClient();
+  //Get Data From Firebase
+  const db = getFirestore(app);
+
+  useEffect(() => {
+    user && getEventList();
+  }, [user]);
+
+  const getEventList = async () => {
+    const q = query(
+      collection(db, "MeetingEvent"),
+      where("createdBy", "==", user?.email)
+    );
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  };
+
   return (
-    <div>
-      <h1>meeting-type</h1>
+    <div className="p-5">
+      <div className="flex flex-col gap-5">
+        <h2 className="font-bold text-3xl">Meeting Event Type </h2>
+        <Input placeholder="Search " className="max-w-xs" />
+        <hr />
+      </div>
     </div>
   );
 };
